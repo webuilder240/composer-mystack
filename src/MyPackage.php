50b338626f93c944a88ef85af8125c3d822e8e6a
Nishio Takuya <littlecub240@gmail.com>
2015-07-25 19:18:18 +0900

    initial commit

---
+++ src/MyPackage.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MyPackage;
+
+
+class MyPackage
+{
+
+}
---
+++ test/MyPackageTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace MyPackageTest\Test;
+
+use PHPUnit_Framework_TestCase;
+
+class MyPackageTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp(){}
+
+    public function tearDown(){}
+}
